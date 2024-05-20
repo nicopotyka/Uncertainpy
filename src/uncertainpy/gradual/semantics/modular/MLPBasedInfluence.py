870f050ea74b2cef4125a1f305dabbfb5b27daf2
Nico Potyka <nicopotyka@gmx.de>
2024-05-20 13:18:14 +0100

    - added MLP-based influence function

---
+++ src/uncertainpy/gradual/semantics/modular/MLPBasedInfluence.py
@@ -0,0 +1,11 @@
+import math
+
+class MLPBasedInfluence:
+    def __init__(self) -> None:
+        pass
+
+    def compute_strength(self, weight, aggregate):
+        return 1/(1 + math.exp(- math.log(weight/(1-weight))) - aggregate)
+
+    def __str__(self) -> str:
+        return __class__.__name__
--- src/uncertainpy/gradual/semantics/modular/__init__.py
+++ src/uncertainpy/gradual/semantics/modular/__init__.py
@@ -3,3 +3,4 @@ from .SumAggregation import *
 from .QuadraticMaximumInfluence import *
 from .EulerBasedInfluence import *
 from .LinearInfluence import *
+from .MLPBasedInfluence import *
